9725ab743a99525aa6ea622150100d1e49563cf0
Sandipan Saha <sandipansaha1998@icloud.com>
2023-06-11 13:41:59 +0530

    Set up DNS and root_url

--- config/enviroment.js
+++ config/enviroment.js
@@ -1,40 +1,42 @@
-const path = require('path');
+const path = require("path");
 // Development Mode Properties
 
 const development = {
-    name:'development',
-    asset_path:'/static' ,
-    session_cookie_key:'crunchy',
-    db:'authentication_development',
-    smtp:{
-        service:'gmail',
-        host:'smtp.gmail.com',
-        port:587,
-        secure:false,
-        auth:{
-            user:process.env.AUTH_SYS_EMAIL,
-            pass:process.env.AUTH_SYS_PASS
-        }
-    }
-}
+  name: "development",
+  root_url: "localhost:8000",
+  asset_path: "/static",
+  session_cookie_key: "crunchy",
+  db: "authentication_development",
+  smtp: {
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.AUTH_SYS_EMAIL,
+      pass: process.env.AUTH_SYS_PASS,
+    },
+  },
+};
 // production Mode Properties
 
 const production = {
-    name:'production',
-    asset_path:process.env.AUTH_SYS_ASSET_PATH ,
-    session_cookie_key:process.env.AUTH_SYS_SESSION_COOKIE_KEY,
-    db:process.env.AUTH_SYS_DB,
-    smtp:{
-        service:'gmail',
-        host:'smtp.gmail.com',
-        port:587,
-        secure:false,
-        auth:{
-            user:process.env.AUTH_SYS_EMAIL,
-            pass:process.env.AUTH_SYS_PASS
-        }
-    }
-}
+  name: "production",
+  root_url: "authentication-system.socialise-india.in",
+  asset_path: process.env.AUTH_SYS_ASSET_PATH,
+  session_cookie_key: process.env.AUTH_SYS_SESSION_COOKIE_KEY,
+  db: process.env.AUTH_SYS_DB,
+  smtp: {
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.AUTH_SYS_EMAIL,
+      pass: process.env.AUTH_SYS_PASS,
+    },
+  },
+};
 
-module.exports = eval(process.env.AUTH_SYS_ENVIROMENT) == undefined ? development:production;
-
+module.exports =
+  eval(process.env.AUTH_SYS_ENVIROMENT) == undefined ? development : production;
--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -1,48 +1,54 @@
-const passport = require('passport');
-const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const crypto = require('crypto');
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
-const env = require('../config/enviroment');
+const passport = require("passport");
+const googleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const crypto = require("crypto");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const env = require("../config/enviroment");
 
-
-passport.use(new googleStrategy({
-    clientID:process.env.AUTH_SYS_GOOGLE_ID,
-    clientSecret:process.env.AUTH_SYS_GOOGLE_CLIENT_SECRET,
-    callbackURL:'http://13.51.255.230.nip.io/user/auth/google/callback'
-},
-    function(accessToken,refreshToken,profile,done){
-        console.log('Inside callback')
-        // Find the user 
-        User.findOne({email:profile.emails[0].value}).then(async (user) =>{
-            if(user)
+passport.use(
+  new googleStrategy(
+    {
+      clientID: process.env.AUTH_SYS_GOOGLE_ID,
+      clientSecret: process.env.AUTH_SYS_GOOGLE_CLIENT_SECRET,
+      callbackURL: `http://${env.root_url}/user/auth/google/callback`,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      console.log("Inside callback");
+      // Find the user
+      User.findOne({ email: profile.emails[0].value })
+        .then(async (user) => {
+          if (user)
             // if found,set this user as req.user
-                return done(null,user);
-            else
-            {
-                // Hashed the password
-                let hashedPassword = await bcrypt.hash(crypto.randomBytes(20).toString('hex'), 10);
-                console.log(hashedPassword)
-            // Create the user and then set this as req.user 
-                User.create({
-                    name:profile.displayName,
-                    email:profile.emails[0].value,
-                    password:hashedPassword,
-                    resetPasswordLinkTime:Date.now()
-                }
-                ).catch(err=>{
-                    console.log('Error in creating a new user',err);
-                    return
-                }).then(user=>{
-                    console.log("Succes in google auth")
-                    return done(null,user);
-                })
-                
-            }
-        }).catch(err => {
-            console.log('Error in google strategy',err);
+            return done(null, user);
+          else {
+            // Hashed the password
+            let hashedPassword = await bcrypt.hash(
+              crypto.randomBytes(20).toString("hex"),
+              10
+            );
+            console.log(hashedPassword);
+            // Create the user and then set this as req.user
+            User.create({
+              name: profile.displayName,
+              email: profile.emails[0].value,
+              password: hashedPassword,
+              resetPasswordLinkTime: Date.now(),
+            })
+              .catch((err) => {
+                console.log("Error in creating a new user", err);
+                return;
+              })
+              .then((user) => {
+                console.log("Succes in google auth");
+                return done(null, user);
+              });
+          }
         })
+        .catch((err) => {
+          console.log("Error in google strategy", err);
+        });
     }
-))
+  )
+);
 
 module.exports = passport;
